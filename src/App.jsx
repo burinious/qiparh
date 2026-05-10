44b58f0f01d8028ac29dd2d859f3ad0f0d86e449
burinious <akomolafeayoyinka@gmail.com>
2026-05-10 21:36:46 +0100

    Opt into React Router future flags

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function ProtectedRoute({ children }) {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/login" element={<AuthPage mode="login" />} />
         <Route path="/register" element={<AuthPage mode="register" />} />
